96effc174c56003c5691ef567ed3f749e5b5b205
laoching <wjdtkdals159@gmail.com>
2022-07-01 22:38:37 +0900

    hide secret key

--- locallibrary/locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/locallibrary/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from http.client import ImproperConnectionState
 from pathlib import Path
+from pathlib import Path
+import os, json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f7$m^&1s5b2hma7p^&ztezzqt&^t&h!8%hlbm+f4zqb)$u^+#0'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting):
+    try:
+        return(secrets[setting])
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperConnectionState(error_msg)
+        
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
